cc08960c10922dece7224a0cff7f53cdd1b15d9a
Bruno Lebon <Bruno.Lebon@brunel.ac.uk>
2022-12-14 09:47:21 +0000

    Moved to pyproject.toml for build and version 0.4 including xls input files.

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ copyright = f"{year}, Bruno Lebon"
 author = "Bruno Lebon"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.4"
 
 
 # -- General configuration ---------------------------------------------------
